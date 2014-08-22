e9a240a60bed80e773e1ec1bcf29a0efffdad437
kaiix <kvn.hou@gmail.com>
2014-08-22 16:52:24 +0800

    Update workon script

--- bash_completion.d/virtualenv_workon.bash
+++ bash_completion.d/virtualenv_workon.bash
@@ -1,22 +1,17 @@
+VENV_HOME=~/.venv
+
 # virtualenv workon
 #---------------------------
 workon ()
 {
-    local venv="/Users/kaiix/.venvs/";
-    local proj="/Users/kaiix/projects/";
-    local codes="/Users/kaiix/codes/";
+    echo $1
+    local venv=${VENV_HOME};
 
-    if [ -d $venv$1 ]; then
-        if [ -d $proj$1 ]; then
-            source $venv$1/bin/activate;
-        elif [ -d $codes$1 ]; then
-            source $venv$1/bin/activate;
-        else
-            source $venv$1/bin/activate;
-        fi
+    if [ -d $venv/$1 ]; then
+        source $venv/$1/bin/activate;
     else
         echo "ERROR $1:";
-        echo $venv$1"bin/activate";
+        echo $venv/$1/bin/activate;
     fi
 }
 
@@ -26,7 +21,7 @@ _workon()
     cur="${COMP_WORDS[COMP_CWORD]}";
     prev="${COMP_WORDS[COMP_CWORD-1]}";
 
-    COMPREPLY=( $(compgen -W "$(ls -f1t ~/.venvs|sed -e 's/[./@]//g' -e '/^$/d')" -- ${cur}) );
+    COMPREPLY=( $(compgen -W "$(ls -f1t ${VENV_HOME}|sed -e 's/[./@]//g' -e '/^$/d')" -- ${cur}) );
 }
 
 complete -o nospace -F _workon workon
