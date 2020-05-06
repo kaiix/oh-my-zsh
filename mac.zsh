f2ea9fe9a019d638fca5368d50cb111ecd6ebb23
kaiix <kvn.hou@gmail.com>
2020-05-06 12:32:00 +0800

    Remove platform specified zsh config

--- mac.zsh
+++
@@ -1,9 +0,0 @@
-# iTerm2
-case $TERM in
-    *xterm*|ansi)
-        function settab { print -Pn "\e]1;${PWD##*/}\a" }
-        function settitle { print -Pn "\e]2;${PWD##*/}\a" }
-        function chpwd { settab;settitle }
-        settab;settitle
-        ;;
-esac
