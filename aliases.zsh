2ce95bc44693b209f9e3f087b33d317179033d91
kaiix <kvn.hou@gmail.com>
2020-05-06 13:22:48 +0800

    Update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,7 +1,7 @@
 alias sz='source ~/.zshrc'
 
 # system
-alias ls='ls -AFG'
+alias ls='exa -aF'
 alias ll='ls -l'
 alias df='df -h'
 alias du='du -h -c'
@@ -14,13 +14,16 @@ alias vi='vim'
 alias vihosts='sudo vim /etc/hosts'
 alias vissh='vim ~/.ssh/config'
 alias virc='vim ~/.vimrc'
+alias vialias="vim $HOME/.oh-my-zsh/aliases.zsh"
 alias rmattr="xattr -r -d com.apple.quarantine"
 alias pr="hub pull-request -o -b"
+alias mr="lab mr create"
 alias mkvenv='python3 -m venv --prompt $(basename $PWD) venv'
 alias help='tldr'
 alias du='ncdu --color dark -rr -x --exclude .git --exclude node_modules'
-alias ping='prettyping --nolegend'
 alias nv='nvim'
+alias jupyterd="docker run --rm -p 9000:8888 -v $HOME/notebook:/home/jovyan jupyter/scipy-notebook:latest"
+alias ping='prettyping --nolegend'
 
 # suffix aliases
 alias -s log="less -MN"
@@ -30,4 +33,9 @@ drop ()
 {
     cp -R $1 ~/Dropbox/$2;
     echo "copy $1 -> Dropbox/$2";
+}
+
+dragon()
+{
+    pip3 install --user -U flake8 autoflake isort black
 }
